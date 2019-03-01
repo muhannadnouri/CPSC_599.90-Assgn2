5294314f989a3e9218f0b4af50bdb21ccea2640c
Muhannad <muhannad_2006@hotmail.com>
2019-02-28 22:11:43 -0700

    Upload Initial Files

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    //destination marker reference
+    public GameObject markerGoal;
+
+    //parent position
+    Vector3 parentPos;
+
+    //navmesh component reference
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToTarget();
+    }
+
+    // function to track position of second marker
+    void MoveToTarget()
+    {
+        if (markerGoal.active)
+        {
+            parentPos = markerGoal.transform.parent.position;
+            agent.SetDestination(parentPos);
+        }
+    }
+}
