d84ae703486988914cde25574897ed04badb5e80
Muhannad <muhannad_2006@hotmail.com>
2019-03-01 23:11:11 -0700

    Upload Files

---
+++ Assets/Scripts/Gyro.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gyro : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // make sure you enable input
+        Input.gyro.enabled = false;
+        Input.gyro.enabled = true;
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+
+    private static Quaternion GyroToUnity(Quaternion q)
+    {
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotate on y-axis using gyroscope
+        Quaternion deviceRotation = GyroToUnity(Input.gyro.attitude);
+        Vector3 deviceEulerAngles = deviceRotation.eulerAngles;
+        Vector3 transformEulerAngles = transform.rotation.eulerAngles;
+        //transform.Rotate((new Vector3(transformEulerAngles.x, deviceEulerAngles.x, transformEulerAngles.z)) * (0.9f * Time.deltaTime));
+        transform.Rotate((new Vector3(0, deviceEulerAngles.x, 0)) * (0.9f * Time.deltaTime));
+    }
+}
---
+++ Assets/Scripts/Sensors.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using EasyAR;
+
+public class Sensors : MonoBehaviour
+{
+    private AndroidJavaObject plugin;
+
+    void Start()
+    {
+#if UNITY_ANDROID
+        plugin = new AndroidJavaClass("jp.kshoji.unity.sensor.UnitySensorPlugin").CallStatic<AndroidJavaObject>("getInstance");
+        plugin.Call("startSensorListening", "accelerometer");
+        plugin.Call("startSensorListening", "light");
+        plugin.Call("startSensorListening", "gyroscope");
+        plugin.Call("startSensorListening", "proximity");
+        plugin.Call("startSensorListening", "pressure");
+#endif
+    }
+
+    void Update()
+    {
+#if UNITY_ANDROID
+        if (plugin != null)
+        {
+            float[] gyroscopeValues = plugin.Call<float[]>("getSensorValues", "gyroscope");
+            float[] proximityValues = plugin.Call<float[]>("getSensorValues", "proximity");
+
+            var kyle = GameObject.Find("Root");
+
+            if (gyroscopeValues != null)
+            {
+                if (gyroscopeValues[1] > 1.0f)
+                {
+                    kyle.transform.Rotate(0, 100 * Time.deltaTime, 0);
+                }
+            }
+
+            if (proximityValues != null)
+            {
+                if (proximityValues[0] < 1.0f)
+                {
+                    kyle.transform.Rotate(100 * Time.deltaTime, 0, 0);
+                }
+
+            }
+        }
+
+#endif
+    }
+
+}
